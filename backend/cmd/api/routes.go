ebea0085525b849f475d6cbbd3feab799c46511b
Divyesh Parikh <dcparikh360@gmail.com>
2026-01-25 15:07:59 +0530

    added the missing route

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -26,6 +26,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/answers", app.createAnswerHandler)
 	//users
 	router.HandlerFunc(http.MethodPost, "/users/", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/users/activated", app.activateUserHandler)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
