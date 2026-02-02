3ddc831b4a3eafb322f981856b4aca1218192bb8
Divyesh Parikh <dcparikh360@gmail.com>
2026-02-02 16:09:55 +0530

    added cross origin request handling

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"khayal/internal/jsonlog"
 	"khayal/internal/mailer"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -39,6 +40,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -67,6 +71,10 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "2e0670ad4cbf91", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "347dd6e21ca913", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Khayal <noreply@example.com>", "SMTP sender")
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
